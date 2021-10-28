9ce23e014e12153e50967b7beb1830b106657dc7
alkanoidev <alkanoidev@gmail.com>
2021-10-28 17:53:29 +0200

    final

--- sw.js
+++ sw.js
@@ -1,29 +1,3 @@
-// var cacheName = 'calculator-pwa';
-// var filesToCache = [
-//   '/',
-//   '/index.html',
-//   '/css/style.css',
-//   '/js/app.js'
-// ];
-
-// /* Start the service worker and cache all of the app's content */
-// self.addEventListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-// });
-
-// /* Serve cached content when offline */
-// self.addEventListener('fetch', function(e) {
-//   e.respondWith(
-//     caches.match(e.request).then(function(response) {
-//       return response || fetch(e.request);
-//     })
-//   );
-// });
-
 var APP_PREFIX = "ApplicationName_"; // Identifier for this app (this needs to be consistent across every cache update)
 var VERSION = "version_01"; // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION;
